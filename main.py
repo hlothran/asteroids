e9bdb41d84ca6b513f1b0d93ac6af3625bb3dcaa
Hlothran <hlothran@yahoo.com>
2024-12-20 21:16:39 -0500

    Added initial pygame loop

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import pygame
 from constants import *
 
 def main():
+    #added for bootdev purposes
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # pygom initialize
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill("black")
+
+        pygame.display.flip()
+
+        clock.tick(60) #limit FPS to 60
 
 if __name__ == "__main__":
     main()
